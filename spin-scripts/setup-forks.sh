3a2e678757344a3353c05e1ed5903d52b9f3e045
Anand Sharma <anasharm@cisco.com>
2018-10-05 17:46:55 -0400

    Pass the token as a command line parameter

--- spin-scripts/setup-forks.sh
+++ spin-scripts/setup-forks.sh
@@ -3,9 +3,17 @@
 # Setup the base folder
 SPINNAKER_DEV="$HOME/dev/spinnaker"
 mkdir -p $SPINNAKER_DEV
-# If you want your existing GitHub repos to be deleted
-GITHUB_PERSONAL_TOKEN="<token>"
 GITHUB_USER_ID="indrayam"
+GITHUB_PERSONAL_TOKEN="${1}"
+if [ -z "$GITHUB_PERSONAL_TOKEN" ]
+then
+      echo "You forgot to pass a personal GitHub access token :-("
+      echo "Why does the script need it? For deleting your existing \"forked\" Spinnaker repos from your GitHub account"
+      echo
+      echo "Usage: setup-forks.sh <github-token>"
+      exit 1
+fi
+
 
 for ms in deck gate fiat clouddriver orca kayenta front50 rosco igor echo halyard; do
     echo "Deleting $r"
