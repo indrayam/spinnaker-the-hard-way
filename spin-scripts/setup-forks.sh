8739fe8aa6ceaf796f9fb90abf3d81c3a77dc336
Anand Sharma <anasharm@cisco.com>
2018-11-05 14:44:23 -0500

    Fixed a nasty bug!

--- spin-scripts/setup-forks.sh
+++ spin-scripts/setup-forks.sh
@@ -18,8 +18,8 @@ fi
 
 
 for ms in deck gate fiat clouddriver orca kayenta front50 rosco igor echo halyard; do
-    echo "Deleting $r"
-    curl -XDELETE -H "Authorization: token $GITHUB_PERSONAL_TOKEN" "https://api.github.com/repos/${GITHUB_USER_ID}/$r"
+    echo "Deleting $ms"
+    curl -XDELETE -H "Authorization: token $GITHUB_PERSONAL_TOKEN" "https://api.github.com/repos/${GITHUB_USER_ID}/$ms"
     echo "Cloning spinnaker/$ms..."
     cd $SPINNAKER_FORK
     rm -rf $ms
